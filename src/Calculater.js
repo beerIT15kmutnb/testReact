a43d62da688f14862efbf567c2017cfce5483e1b
beerIT15kmutnb <beerde789@hotmail.com  git config --global user.email beerde789@hotmail.com>
2018-05-09 00:32:38 +0700

    final

--- src/Calculater.js
+++ src/Calculater.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDataGrid from 'react-data-grid';
 import Moment from 'react-moment';
 import 'moment-timezone';
+import './App.css';
 class Calculater extends Component {
     constructor() {
         super()
@@ -9,6 +10,7 @@ class Calculater extends Component {
             date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
         this._columns = [
             { key: 'id', name: 'งวดที่' },
+            { key: 'date', name: 'วันที่' },
             { key: 'ans', name: 'เงินกู้/เงินกู้คงเหลือ' },
             { key: 'ratesum', name: 'ดอกเบี้ย' },
             { key: 'yod', name: 'เงินต้น' },
@@ -16,6 +18,7 @@ class Calculater extends Component {
         this._rows = []
         this._sumRate = 0
         this._count = 0
+        this._round = 0
         this.state = {
             old: 0,
             rat: 0,
@@ -30,27 +33,37 @@ class Calculater extends Component {
 
     createRows = () => {
         let rows = [];
+        this._sumRate = 0
+        this._count = 0
         let round = (60 - +this.state.old) * 12
+        this._round = round
         let yod = (Math.ceil(Math.ceil(+this.state.yod / round) / 10)) * 10
         let last = yod - ((yod * round) - +this.state.yod)
         let ans = +this.state.count
         let count = 0
         let ratesum = 0
+        let date = new Date()
+        let y = 0
+        let m = date.getMonth()+1
         for (let i = 0; i < round; i++) {
-            ratesum = (+ans * (+this.state.rat / 100) / 365) * 30
-            count = yod + ratesum
+           
+            ratesum = (+ans * (+this.state.rat / 100) / 365) * this.daysInThisMonth(date.getFullYear()+y,m)
             if(i === round -1) yod = last
+            count = yod + ratesum
             rows.push({
                 id: i + 1,
-                ratesum: ratesum,
-                count: count,
-                ans: ans,
-                yod: yod
+                ratesum: Math.round( ratesum*100)/100,
+                count: Math.round(  count*100)/100,
+                ans: Math.round(  ans*100)/100,
+                yod: Math.round(  yod*100)/100,
+                date: this.daysInThisMonth(date.getFullYear()+y,m) +'/'+ m + '/' + (+date.getFullYear() + y)
             });
             ans = ans - yod
-            this._sumRate +=ratesum
-            this._count +=count
-            console.log({ i: i, เงินกู้คงเหลือ: ans, ดอกเบี้ย: ratesum, เงินต้น: yod, รวมหัก: count ,_sumRate:this._sumRate,_count:this._count});
+            this._sumRate += ratesum
+            this._count += count
+            console.log({ i: i, 'เงินกู้คงเหลือ': ans, 'ดอกเบี้ย': ratesum, 'เงินต้น': yod, 'รวมหัก': count ,_sumRate:this._sumRate,_count:this._count});
+            m = m - 12 === 0 ? 1 : m + 1
+            y = m === 1 ? y+1 :y
         }
         console.log(rows);
 
@@ -60,7 +73,9 @@ class Calculater extends Component {
     rowGetter = (i) => {
         return this._rows[i];
     };
-
+    daysInThisMonth(y,m) {
+        return new Date(y, m, 0).getDate();
+      }
     result() {
         this.createRows();
         this.setState({ operater: true })
@@ -85,15 +100,16 @@ class Calculater extends Component {
 
                 <button onClick={this.result.bind(this)}> คำนวณเงินกู้ </button>
                 <br />
-                <div >
+                <div className="grid" >
                     <ReactDataGrid
                         columns={this._columns}
                         rowGetter={this.rowGetter}
                         rowsCount={this._rows.length}
                     />
                 </div>
-                
-                <h3>รวมดอก: </h3>
+                <h3>จำนวนงวดผ่อนชำระ: { this._round} งวด</h3>
+                <h3>รวมดอกเบี้ย: { Math.round(  this._sumRate*100)/100} บาท</h3>
+                <h3>ยอดรวมจ่าย: { Math.round(  this._count*100)/100} บาท</h3>
             </div>
         )
     }
